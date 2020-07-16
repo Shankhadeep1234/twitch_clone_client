0957f8281599a5338400336279767979663cf4a5
Shankhadeep Bhadra <shankhadeepraj@gmail.com>
2020-07-16 18:56:52 +0530

    Update streamReducer.js

--- src/reducers/streamReducer.js
+++ src/reducers/streamReducer.js
@@ -1,24 +1,24 @@
-import _ from "lodash";
+import _ from 'lodash';
 import {
-  CREATE_STREAM,
   FETCH_STREAM,
   FETCH_STREAMS,
-  DELETE_STREAM,
-  EDIT_STREAM
-} from "../actions/types";
+  CREATE_STREAM,
+  EDIT_STREAM,
+  DELETE_STREAM
+} from '../actions/types';
 
 export default (state = {}, action) => {
   switch (action.type) {
-    case CREATE_STREAM:
-      return { ...state, [action.payload.id]: action.payload };
+    case FETCH_STREAMS:
+      return { ...state, ..._.mapKeys(action.payload, 'id') };
     case FETCH_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case CREATE_STREAM:
       return { ...state, [action.payload.id]: action.payload };
     case EDIT_STREAM:
       return { ...state, [action.payload.id]: action.payload };
     case DELETE_STREAM:
       return _.omit(state, action.payload);
-    case FETCH_STREAMS:
-      return { ...state, ..._.mapKeys(action.payload, "id") };
     default:
       return state;
   }
